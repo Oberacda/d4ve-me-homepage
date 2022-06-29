ed9164b6321696650f5712a4a33d09eb3d609f70
David Oberacker <doberacker@d4ve.me>
2022-06-29 23:15:03 +0200

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+    colors: {
+        'ivory': '#f6f7ebff',
+        'cinnabar': '#e94f37ff',
+        'onyx': '#393e41ff',
+        'green-blue-crayola': '#3f88c5ff',
+        'keppel': '#44bba4ff',
+    }
+  },
+  plugins: [],
+}
